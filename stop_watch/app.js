b6baa8e5eaa87847665d6c58e266152cf50c1696
chmini <beelzebule@naver.com>
2021-11-29 18:05:22 +0900

    05.Stop watch

--- stop_watch/app.js
+++ stop_watch/app.js
@@ -1,34 +1,65 @@
 const $display = document.querySelector('.display');
-const $controlBtns = document.querySelectorAll('.control');
+const $btn1 = document.querySelector('.control');
+const $laps = document.querySelector('.laps');
 
 let ms = 0;
 let ss = 0;
 let mm = 0;
 
+let num = 0;
+
 let timer;
 
-const getMsString = ms => {
-  const modms = ms % 100;
-  return modms < 9 ? '0' + modms : modms;
+const laps = [];
+
+const format = time => (time < 10 ? `0${time}` : time);
+
+$btn1.onclick = e => {
+  if (e.target.textContent === 'Start') {
+    $btn1.textContent = 'Stop';
+
+    timer = setInterval(() => {
+      ms++;
+
+      if (ms > 99) {
+        ss++;
+        ms = 0;
+      }
+
+      if (ss > 59) {
+        mm++;
+        ss = 0;
+      }
+
+      $display.textContent = `${format(mm)}:${format(ss)}:${format(ms)}`;
+    }, 10);
+
+    $btn1.nextElementSibling.disabled = false;
+    $btn1.nextElementSibling.textContent = 'Laps';
+
+    $btn1.nextElementSibling.onclick = e => {
+      if (e.target.textContent !== 'Laps') return;
+      $laps.innerHTML += `${
+        num === 0 ? '<div class="lap-title">Laps</div><div class="lap-title">Time</div>' : ''
+      }<div>${++num}</div>
+      <div>${format(mm)}:${format(ss)}:${format(ms)}</div>`;
+    };
+  } else {
+    $btn1.textContent = 'Start';
+
+    clearInterval(timer);
+
+    $btn1.nextElementSibling.textContent = 'Reset';
+
+    $btn1.nextElementSibling.onclick = e => {
+      if (e.target.textContent !== 'Reset') return;
+      $display.textContent = '00:00:00';
+      e.target.disabled = true;
+      mm = 0;
+      ss = 0;
+      ms = 0;
+      $laps.innerHTML = '';
+      num = 0;
+    };
+  }
 };
-
-$controlBtns.forEach($btn => {
-  $btn.onclick = e => {
-    if (e.target.textContent === 'Start') {
-      $btn.textContent = 'Stop';
-      timer = setInterval(() => {
-        ms++;
-
-        ss = Math.floor(ms / 100);
-
-        mm = Math.floor(ss / 60);
-        mm = mm > 9 ? mm : '0' + mm;
-
-        $display.textContent = `${mm}:${ss}:${getMsString(ms)}`;
-      }, 10);
-    } else {
-      $btn.textContent = 'Start';
-      clearInterval(timer);
-    }
-  };
-});
