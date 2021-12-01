3debe72e67520e31f443a0069a8f7c1ef2e13e55
zeromountain <sonyeongsan@gmail.com>
2021-12-01 16:13:05 +0900

    09. Accordion

--- accordion/app.js
+++ accordion/app.js
@@ -4,10 +4,11 @@ const $accordion = document.querySelector('.accordion');
   $menuContainer.children[1].style.height = $menuContainer.classList.contains('active')
     ? `${$menuContainer.children[1].scrollHeight}px`
     : '0';
+  if ($menuContainer.classList.contains('active')) $menuContainer.children[1].classList.add('notransition');
 });
 
 $accordion.onclick = e => {
-  if (!(e.target.matches('.menu') || e.target.matches('.menu *'))) return;
+  if (!(e.target.matches('.menu') || !e.target.matches('.menu *'))) return;
 
   [...$accordion.children].forEach($menuContainer => {
     $menuContainer.classList.toggle('active', e.target.parentNode === $menuContainer);
@@ -17,4 +18,8 @@ $accordion.onclick = e => {
     $menuContainer.children[1].style.height =
       e.target.parentNode === $menuContainer ? `${e.target.nextElementSibling.scrollHeight}px` : '0';
   });
+};
+
+window.onload = () => {
+  [...$accordion.children].forEach($menuContainer => $menuContainer.children[1].classList.remove('notransition'));
 };
