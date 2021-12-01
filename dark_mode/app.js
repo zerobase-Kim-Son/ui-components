d25c638e5ea7ed3d2cd77e3c1666144579cdb35a
chmini <beelzebule@naver.com>
2021-12-01 15:27:13 +0900

    06.Dark Mode

--- dark_mode/app.js
+++ dark_mode/app.js
@@ -1,25 +1,27 @@
 const $toggleButton = document.querySelector('.toggle-button');
 
 const getTheme = () => localStorage.getItem('theme');
+
 const setTheme = theme => localStorage.setItem('theme', theme);
 
 const initialTheme = () => {
   if (getTheme()) return;
+
   window.matchMedia('(prefers-color-scheme: dark)').matches ? setTheme('dark') : setTheme('light');
 };
 
 const render = () => {
   initialTheme();
+
   if (getTheme() !== 'dark') return;
+
   document.body.classList.add('dark');
 };
 
-// render();
-
-$toggleButton.onclick = e => {
+$toggleButton.onclick = () => {
   document.body.classList.toggle('dark');
 
   document.body.classList.contains('dark') ? setTheme('dark') : setTheme('light');
 };
 
-window.addEventListener('DOMContentLoaded', render);
+render();
