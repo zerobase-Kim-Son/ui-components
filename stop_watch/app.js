965edcd9e59722fee940aa500e294f6e59e8e3ca
chmini <beelzebule@naver.com>
2021-12-01 14:15:03 +0900

    05.Stop Watch

--- stop_watch/app.js
+++ stop_watch/app.js
@@ -1,64 +1,65 @@
 const $display = document.querySelector('.display');
-const $btn1 = document.querySelector('.control');
+const $controlBtn = document.querySelector('.control');
 const $laps = document.querySelector('.laps');
-
-let ms = 0;
-let ss = 0;
-let mm = 0;
-
-let num = 0;
-
-let timer;
-
-const laps = [];
 
 const format = time => (time < 10 ? `0${time}` : time);
 
-$btn1.onclick = e => {
+const time = { mm: 0, ss: 0, ms: 0 };
+
+$controlBtn.onclick = e => {
+  let timer;
+  let num = 0;
   if (e.target.textContent === 'Start') {
-    $btn1.textContent = 'Stop';
+    // element
+    $controlBtn.textContent = 'Stop';
+    $controlBtn.nextElementSibling.disabled = false;
+    $controlBtn.nextElementSibling.textContent = 'Laps';
 
+    // timer
     timer = setInterval(() => {
-      ms++;
+      time.ms++;
 
-      if (ms > 99) {
-        ss++;
-        ms = 0;
+      if (time.ms > 99) {
+        time.ss++;
+        time.ms = 0;
       }
 
-      if (ss > 59) {
-        mm++;
-        ss = 0;
+      if (time.ss > 59) {
+        time.mm++;
+        time.ss = 0;
       }
 
-      $display.textContent = `${format(mm)}:${format(ss)}:${format(ms)}`;
+      $display.textContent = `${format(time.mm)}:${format(time.ss)}:${format(time.ms)}`;
     }, 10);
 
-    $btn1.nextElementSibling.disabled = false;
-    $btn1.nextElementSibling.textContent = 'Laps';
+    // laps
+    $controlBtn.nextElementSibling.onclick = () => {
+      $laps.innerHTML +=
+        num === 0
+          ? '<div class="lap-title">Laps</div><div class="lap-title">Time</div>'
+          : `<div>${num}</div><div>${format(time.mm)}:${format(time.ss)}:${format(time.ms)}</div>`;
 
-    $btn1.nextElementSibling.onclick = e => {
-      if (e.target.textContent !== 'Laps') return;
-      $laps.innerHTML += `${
-        num === 0 ? '<div class="lap-title">Laps</div><div class="lap-title">Time</div>' : ''
-      }<div>${++num}</div>
-      <div>${format(mm)}:${format(ss)}:${format(ms)}</div>`;
+      num++;
     };
   } else {
-    $btn1.textContent = 'Start';
+    // element
+    $controlBtn.textContent = 'Start';
+    $controlBtn.nextElementSibling.textContent = 'Reset';
 
+    // timer
     clearInterval(timer);
 
-    $btn1.nextElementSibling.textContent = 'Reset';
-
-    $btn1.nextElementSibling.onclick = e => {
-      if (e.target.textContent !== 'Reset') return;
+    // reset
+    $controlBtn.nextElementSibling.onclick = e => {
       $display.textContent = '00:00:00';
       e.target.disabled = true;
-      mm = 0;
-      ss = 0;
-      ms = 0;
+
+      time.mm = 0;
+      time.ss = 0;
+      time.ms = 0;
+
       $laps.innerHTML = '';
+
       num = 0;
     };
   }
