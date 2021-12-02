cdcbb4a92b4b9a314003f805683ab58745a9f747
chmini <beelzebule@naver.com>
2021-12-02 12:15:03 +0900

    05.Stop Watch

--- stop_watch/app.js
+++ stop_watch/app.js
@@ -1,66 +1,87 @@
 const $display = document.querySelector('.display');
-const $controlBtn = document.querySelector('.control');
+const $firstControlBtn = document.querySelector('.control');
 const $laps = document.querySelector('.laps');
 
-const format = time => (time < 10 ? `0${time}` : time);
+let time = { mm: 0, ss: 0, ms: 0 };
+let timer;
 
-const time = { mm: 0, ss: 0, ms: 0 };
+const format = time => (time < 10 ? `0${time}` : `${time}`);
 
-$controlBtn.onclick = e => {
-  let timer;
-  let num = 0;
-  if (e.target.textContent === 'Start') {
-    // element
-    $controlBtn.textContent = 'Stop';
-    $controlBtn.nextElementSibling.disabled = false;
-    $controlBtn.nextElementSibling.textContent = 'Laps';
+const getRandomNum = () => Math.floor(Math.random() * 9 + 1);
 
-    // timer
-    timer = setInterval(() => {
-      time.ms++;
+const timeDisplay = randomNum => {
+  const ms = randomNum ? format(time.ms).slice(0, 1) + randomNum : format(time.ms);
+  $display.textContent = `${format(time.mm)}:${format(time.ss)}:${ms}`;
+};
 
-      if (time.ms > 99) {
-        time.ss++;
-        time.ms = 0;
-      }
+let randomNum = getRandomNum();
 
-      if (time.ss > 59) {
-        time.mm++;
-        time.ss = 0;
-      }
+const start = () =>
+  setInterval(() => {
+    time.ms++;
 
-      $display.textContent = `${format(time.mm)}:${format(time.ss)}:${format(time.ms)}`;
-    }, 10);
-
-    // laps
-    $controlBtn.nextElementSibling.onclick = () => {
-      $laps.innerHTML +=
-        num === 0
-          ? '<div class="lap-title">Laps</div><div class="lap-title">Time</div>'
-          : `<div>${num}</div><div>${format(time.mm)}:${format(time.ss)}:${format(time.ms)}</div>`;
-
-      num++;
-    };
-  } else {
-    // element
-    $controlBtn.textContent = 'Start';
-    $controlBtn.nextElementSibling.textContent = 'Reset';
-
-    // timer
-    clearInterval(timer);
-
-    // reset
-    $controlBtn.nextElementSibling.onclick = e => {
-      $display.textContent = '00:00:00';
-      e.target.disabled = true;
-
-      time.mm = 0;
-      time.ss = 0;
+    if (time.ms > 99) {
+      time.ss++;
       time.ms = 0;
+    }
 
+    if (time.ss > 59) {
+      time.mm++;
+      time.ss = 0;
+    }
+
+    timeDisplay(randomNum);
+  }, 10);
+
+const stop = timer => {
+  randomNum = getRandomNum();
+  clearInterval(timer);
+
+  timeDisplay();
+};
+
+$firstControlBtn.onclick = ({ target }) => {
+  let lapNum = 1;
+
+  const $secondControlBtn = target.nextElementSibling;
+
+  const toggleBtn = state => {
+    if (state === 'Start') {
+      target.textContent = 'Stop';
+
+      $secondControlBtn.textContent = 'Laps';
+      $secondControlBtn.disabled = false;
+
+      timer = start();
+    } else {
+      target.textContent = 'Start';
+
+      $secondControlBtn.textContent = 'Reset';
+
+      stop(timer);
+    }
+  };
+
+  toggleBtn(target.textContent);
+
+  $secondControlBtn.onclick = ({ target }) => {
+    if (target.textContent === 'Reset') {
+      time = { mm: 0, ss: 0, ms: 0 };
+      $display.textContent = '00:00:00';
+      target.disabled = true;
+      lapNum = 0;
       $laps.innerHTML = '';
+    }
 
-      num = 0;
-    };
-  }
+    if (target.textContent === 'Laps') {
+      if (!$laps.innerHTML) $laps.innerHTML += '<div class="lap-title">Laps</div><div class="lap-title">Time</div>';
+
+      $laps.innerHTML += `<div>${lapNum}</div><div>${format(time.mm)}:${format(time.ss)}:${format(time.ms)}</div>`;
+
+      lapNum++;
+
+      randomNum = getRandomNum();
+      timeDisplay(randomNum);
+    }
+  };
 };
