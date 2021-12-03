e71de989207b84540bbc268e6f6c9c618f4e6629
zeromountain <sonyeongsan@gmail.com>
2021-12-03 12:07:11 +0900

    13.form_validation

--- form_validation/js/auth.js
+++ form_validation/js/auth.js
@@ -1,52 +1,25 @@
-import { checkUserid, checkUserName, checkPassword } from './checkValidation.js';
-import toaster from './toaster.js';
+import { renderIcon } from './renderIcon.js';
+import { hideForm } from './hideForm.js';
+import { toaster, createToastAction } from './toaster.js';
 
 const $forms = document.querySelectorAll('.form');
-const [$signin, $signup] = [...$forms];
 
-const createToastAction = (type, title, message) => ({ type, title, message });
+// TODO: POST 요청
+[...$forms].forEach($form => {
+  $form.addEventListener('submit', e => {
+    e.preventDefault();
 
-// TODO: submit 이벤트 처리, POST 요청
-$signin.addEventListener('submit', e => {
-  e.preventDefault();
-  toaster.add(createToastAction('success', 'Well done!', 'Signin Successfully'));
+    $form.classList.contains('signin')
+      ? toaster.add(createToastAction('success', 'Well done!', 'Signin Successfully'))
+      : toaster.add(createToastAction('success', 'Well done!', 'Signup Successfully'));
+  });
+
+  $form.addEventListener('input', ({ target }) => {
+    const $submitBtn = $form.querySelector('.button');
+    renderIcon(target, $submitBtn, $form);
+  });
+
+  $form.addEventListener('click', ({ target }) => {
+    hideForm(target, $form);
+  });
 });
-
-$signup.addEventListener('submit', e => {
-  e.preventDefault();
-  toaster.add(createToastAction('success', 'Well done!', 'Signup Successfully'));
-});
-
-$signin.oninput = ({ target }) => {
-  const $submitBtn = $signin.querySelector('.button');
-  if (target.id === 'signin-userid') checkUserid(target);
-  if (target.id === 'signin-password') checkPassword(target);
-  const $successIcons = [...$signin.querySelectorAll('.icon-success')];
-  const countSuccessIcons = [...$successIcons].filter($successIcon => $successIcon.classList.contains('hidden')).length;
-
-  countSuccessIcons === 0 ? ($submitBtn.disabled = false) : ($submitBtn.disabled = true);
-};
-
-$signup.oninput = ({ target }) => {
-  const $submitBtn = $signup.querySelector('.button');
-  if (target.id === 'signup-userid') checkUserid(target);
-  if (target.id === 'signup-name') checkUserName(target);
-  if (target.id === 'signup-password' || target.id === 'signup-confirm-password') checkPassword(target);
-
-  const $successIcons = [...$signup.querySelectorAll('.icon-success')];
-  const countSuccessIcons = [...$successIcons].filter($successIcon => $successIcon.classList.contains('hidden')).length;
-
-  countSuccessIcons === 0 ? ($submitBtn.disabled = false) : ($submitBtn.disabled = true);
-};
-
-$signin.onclick = ({ target }) => {
-  if (!target.matches('.link > a')) return;
-  $signin.classList.add('hidden');
-  $signup.classList.remove('hidden');
-};
-
-$signup.onclick = ({ target }) => {
-  if (!target.matches('.link > a')) return;
-  $signup.classList.add('hidden');
-  $signin.classList.remove('hidden');
-};
