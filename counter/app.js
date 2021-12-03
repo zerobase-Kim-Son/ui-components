75d4de32633e660a05e6c6d8d6ff65da1ac98c3e
chmini <beelzebule@naver.com>
2021-12-03 15:48:32 +0900

    03.Counter

--- counter/app.js
+++ counter/app.js
@@ -2,22 +2,22 @@ const $increaseBtn = document.querySelector('.increase');
 const $decreaseBtn = document.querySelector('.decrease');
 const $counter = document.querySelector('.counter');
 
+const increase = count => count + 1;
+
+const decrease = count => (count < 1 ? 0 : count - 1);
+
 const counter = (() => {
   let count = 0;
-  return {
-    increase() {
-      return ++count;
-    },
-    decrease() {
-      return count > 0 ? --count : 0;
-    },
+  return operation => {
+    count = operation(count);
+    return count;
   };
 })();
 
 $increaseBtn.onclick = () => {
-  $counter.textContent = counter.increase();
+  $counter.textContent = counter(increase);
 };
 
 $decreaseBtn.onclick = () => {
-  $counter.textContent = counter.decrease();
+  $counter.textContent = counter(decrease);
 };
